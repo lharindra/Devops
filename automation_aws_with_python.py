cf0e2c2e4efda6178ab01d61180aa3cde6719a5f
lharindra <43954033+lharindra@users.noreply.github.com>
2019-09-09 23:32:49 +0530

    Update automation_aws_with_python.py

--- automation_aws_with_python.py
+++ automation_aws_with_python.py
@@ -1,5 +1,7 @@
 #!/usr/local/bin/python3
 import boto3
+import subprocess
+import textwrap
 
 class main_class():
     def __init__(self):
@@ -9,32 +11,29 @@ class main_class():
         self.inst_running = []
         self.inst_term = []
         self.inst_stop = []
+        self.all_inst_dict_tolist = []
 
     def ec2_list_of_instances(self):
-        ec2_responce = self.ec2_conn_list.describe_instances()
+        ec2_responce = self.ec2_conn_list.describe_instances()  
         for reservation in ec2_responce["Reservations"]:
             for instance in reservation["Instances"]:
-                inst_id = instance["InstanceId"]
-                if instance["State"]["Name"] == "running":
-                    self.total_inst_list[inst_id] = "Running"
-                elif instance["State"]["Name"] == "terminated":
-                    self.total_inst_list[inst_id] = "Terminated"
-                elif instance["State"]["Name"] == "stopped":
-                    self.total_inst_list[inst_id] = "Stopped"
-        for k, v in self.total_inst_list.items():
-            if v == "Running":
-                self.inst_running.append(k)
-            elif v == "Terminated":
-                self.inst_term.append(k)
-            elif v == "Stopped":
-                self.inst_stop.append(k)
-        if len(self.inst_running) != 0:
-                print("Running:- {0}".format(",".join(self.inst_running)))
-        if len(self.inst_term) != 0:
-                print("Terminated:- {0}".format(",".join(self.inst_term)))
-        if len(self.inst_stop) != 0:
-                print("Stopped:- {0}".format(",".join(self.inst_stop)))
-        
+                each_inst_dict = {}
+                each_inst_dict["Sate"] = instance["State"]["Name"]
+                each_inst_dict["InstanceId"] = instance["InstanceId"]
+                self.all_inst_dict_tolist.append(each_inst_dict)
+        columns = list(self.all_inst_dict_tolist[0].keys()) 
+        columns.insert(0, "No.")   
+        tb = SimpleTable(columns)
+        row_number = 0
+        for i in self.all_inst_dict_tolist:
+            row_data = []
+            row_number += 1
+            row_data.append(str(row_number))
+            for k, v in i.items():
+                row_data.append(v)
+            tb.add_row(row_data)
+        tb.print_table()
+
     def creat_new_ec2_instances(self):
         instances = self.ec2_conn_create.create_instances(
             ImageId = 'ami-00fc224d9834053d6',
@@ -50,7 +49,57 @@ class main_class():
     def start_instance(self):
         ec2_start = self.ec2_conn_list.start_instances(InstanceIds = ['i-01e4a3f4999b0ca13'])
 
-    #def show_and_choose(self):
+class SimpleTable():
+
+    def __init__(self, columns=[], wrap_length=20, coldelim=" | ", rowdelim="-"):
+        self.table_heading = columns
+        self.table_rows = []
+        self.rowdelim = rowdelim
+        self.coldelim = coldelim
+        self.wrap_length = wrap_length
+        # self.column_limit = 185
+        # computes terminal width
+        self.column_limit = int(subprocess.Popen(
+            ["tput", "cols"], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").strip("\n"))
+
+    def add_row(self, row_data):
+        self.table_rows.append(row_data)
+
+    def print_table(self):
+        self.set_table_attributes()
+        string = self.rowdelim
+        self.table_rows.insert(0, self.table_heading)
+        # restructure to get textwrap.wrap output for each cell
+        l = [[textwrap.wrap(col, self.wrap_length) for col in row]
+             for row in self.table_rows]
+        for row in l:
+            for n in range(max([len(i) for i in row])):
+                string += self.coldelim
+                for col in row:
+                    if n < len(col):
+                        string += col[n].ljust(self.wrap_length)
+                    else:
+                        string += " " * self.wrap_length
+                    string += self.coldelim
+                string += "\n"
+            string += self.rowdelim
+        print(string)
+
+    def set_table_attributes(self):
+        # considering column delims that come in between table contents
+        p = len(self.coldelim) * (len(self.table_rows[0]) - 1)
+        self.wrap_length = self.check_wrap_length(self.wrap_length, p)
+        self.rowdelim = self.coldelim + self.rowdelim * \
+            (self.wrap_length *
+             max([len(i) for i in self.table_rows]) + p) + self.coldelim + "\n"
+
+    def check_wrap_length(self, wrap_length, p):
+        # if wrap_length * len(self.table_heading) <= self.column_limit - 2 *
+        # len(self.coldelim) - p:
+        if wrap_length * len(self.table_heading) <= self.column_limit * 3 // 4:
+            return wrap_length
+        else:
+            return self.check_wrap_length(wrap_length - 1, p)
 
 if __name__ == "__main__":
     Mainobj = main_class()
